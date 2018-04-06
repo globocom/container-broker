ed5f4c35023e45a2971145fb51af58b8ad0692ce
Joao Vieira <joaocv3@gmail.com>
2018-04-06 16:16:30 -0300

    feature: Create show and create routes for job

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :jobs, only: [:create, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   post "/handshake", controller: "handshake", action: "create"
