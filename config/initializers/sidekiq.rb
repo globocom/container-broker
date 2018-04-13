69c14dcb092603d95e6f57dbd38e44a95b6ebd8d
Douglas Lise <douglaslise@gmail.com>
2018-04-13 14:09:03 -0300

    chore: Move sidekiq config to settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Rails.application.config.active_job.queue_adapter = :sidekiq
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: "redis://localhost:6379/0/k9s" }
+  config.redis = {url: Settings.sidekiq_url }
   # config.redis = {url: "redis://localhost:6379/0/k9s", concurrency: 1}
 end
 
