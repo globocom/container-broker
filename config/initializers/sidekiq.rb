b7c628a7d38f0dfb834f908501c1de521ea4b565
Douglas Lise <douglaslise@gmail.com>
2018-04-12 22:07:15 -0300

    chore: Sidekiq settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,1 +1,8 @@
 Rails.application.config.active_job.queue_adapter = :sidekiq
+
+Sidekiq.configure_client do |config|
+  config.redis = {url: "redis://localhost:6379/0/k9s" }
+  # config.redis = {url: "redis://localhost:6379/0/k9s", concurrency: 1}
+end
+
+Sidekiq.default_worker_options = { retry: 0, backtrace: 10 }
