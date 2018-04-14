d22ae40bfcc83c216ee5d45dd3bb4ea147210d06
Douglas Lise <douglaslise@gmail.com>
2018-04-14 00:29:05 -0300

    chore: Change application name

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Globernetes
+module ContainerBroker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
