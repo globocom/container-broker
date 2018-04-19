728e03c26a094bac8ac3fd92e522c23ac0abc820
Douglas Lise <douglaslise@gmail.com>
2018-04-19 15:02:28 -0300

    feature: Create own lock manager - redlock did not work

--- app/services/lock_manager.rb
+++ app/services/lock_manager.rb
@@ -1,12 +1,44 @@
 class LockManager
-  include Singleton
-  attr_reader :manager
+  attr_reader :redis, :resource, :expire, :wait, :locked, :key
 
-  def initialize
-    @manager = Redlock::Client.new([ENV["DBAAS_REDIS_ENDPOINT"] || "redis://localhost:6379/0/k9s"])
+  def initialize(type:, id:, expire: 60, wait: true)
+    @key = "lockmanager-#{type}-#{id}"
+    @redis = Redis.new(url: Settings.redis_url)
+    @resource = resource
+    @expire = expire
+    @wait = wait
   end
 
-  def self.lock(resource, ttl, &block)
-    instance.manager.lock(resource, ttl, &block)
+  def lock
+    if wait
+      while !locked do
+        sleep 0.1
+        @locked = redis_set(nx: true)
+      end
+    else
+      @locked = redis_set(nx: true)
+    end
+
+    if locked
+      return yield(self)
+    else
+      false
+    end
+  ensure
+    redis.del(key)
+  end
+
+  def keep_locked
+    raise "Lock not aquired" unless locked
+
+    if redis_set(xx: true)
+      puts "lock extended by #{expire}"
+    else
+      raise "Lock expired"
+    end
+  end
+
+  def redis_set(options)
+    redis.set(key, resource, options.merge(ex: expire))
   end
 end
