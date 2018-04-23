8a9a977554b16b2243b4240a84512859c9640696
Douglas Lise <douglaslise@gmail.com>
2018-04-23 18:09:11 -0300

    test: Tests for UpdaateAllNodesStatusJob

--- spec/jobs/update_all_nodes_status_job_spec.rb
+++ spec/jobs/update_all_nodes_status_job_spec.rb
@@ -1,2 +1,18 @@
 require 'rails_helper'
 
+RSpec.describe UpdateAllNodesStatusJob, type: :job do
+  let(:node1) { Node.create!(available: true) }
+  let(:node2) { Node.create!(available: false) }
+  let(:node3) { Node.create!(available: true) }
+
+  let!(:nodes) { [node1, node2, node3] }
+
+  it "schedule UpdateNodeStatusJob for every available node" do
+    subject.perform
+
+    expect(UpdateNodeStatusJob).to have_been_enqueued.with(node: node1)
+    expect(UpdateNodeStatusJob).to have_been_enqueued.with(node: node3)
+
+    expect(UpdateNodeStatusJob).to_not have_been_enqueued.with(node: node2)
+  end
+end
