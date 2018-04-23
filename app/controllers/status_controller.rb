af842f15ed9158190b27ee94635750508ab58655
Douglas Lise <douglaslise@gmail.com>
2018-04-23 17:46:00 -0300

    chore: Status page task count

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,6 +1,6 @@
 class StatusController < ApplicationController
   def index
     @nodes = Node.includes(:slots)
-    @tasks = Task.includes(slot: :node)
+    @tasks = Task.includes(slot: :node).to_a
   end
 end
