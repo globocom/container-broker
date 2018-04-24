b5a2c8b1dd521f04dbfc091b289824193b30c15d
Douglas Lise <douglaslise@gmail.com>
2018-04-24 10:46:55 -0300

    test: Tests for ReleaseSlotJob

--- spec/jobs/release_slot_job_spec.rb
+++ spec/jobs/release_slot_job_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe ReleaseSlotJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:node) { Node.create!(hostname: "local.test" ) }
+  let(:slot) { Slot.create!(node: node, status: "releasing", current_task: task, container_id: container_id)}
+  let(:task) { Task.create! }
+  let(:container_id) { "11223344" }
+
+  let(:perform) { subject.perform(slot: slot) }
+
+  before do
+    allow_any_instance_of(UpdateTaskStatusJob).to receive(:perform)
+  end
+
+  it "updates task status" do
+    expect(UpdateTaskStatusJob).to receive(:perform_now).with(task)
+    perform
+  end
+
+  it "schedules container removal from docker" do
+    perform
+    expect(RemoveContainerJob).to have_been_enqueued.with(node: slot.node, container_id: container_id)
+  end
+
+  it "releases the slot" do
+    expect{perform}.to change(slot, :status).to("idle")
+  end
+
+  it "enqueues new tasks" do
+    perform
+    expect(RunTasksJob).to have_been_enqueued#.with(node: slot.node, conainer_id: slot.container_id)
+  end
 end
