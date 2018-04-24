f899fa76739160427a44dece1f099af683363d31
Douglas Lise <douglaslise@gmail.com>
2018-04-23 21:28:21 -0300

    test: Node model test

--- spec/models/node_spec.rb
+++ spec/models/node_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Node, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#update_usage" do
+    before do
+      Slot.create(node: subject, status: "running")
+      Slot.create(node: subject, status: "attaching")
+      Slot.create(node: subject, status: "idle")
+      Slot.create(node: subject, status: "releasing")
+    end
+
+    it "sets usage to percentual of used slots" do
+      expect{subject.update_usage}.to change(subject, :usage_percent).to(75)
+    end
+  end
 end
