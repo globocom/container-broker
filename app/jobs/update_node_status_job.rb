18dd2ca893b48d478c1d194d93120b8b35a6fd4a
Douglas Lise <douglaslise@gmail.com>
2018-05-21 18:04:00 -0300

    feature: Remove LockManager from UpdateNodeStatusJob that is now controlled by SidekiqUniqueJobs

--- app/jobs/update_node_status_job.rb
+++ app/jobs/update_node_status_job.rb
@@ -4,48 +4,46 @@ class UpdateNodeStatusJob < DockerConnectionJob
   def perform(node:)
     @node = node
 
-    LockManager.new(type: "monitor_node", id: node.id, expire: 1.minute, wait: false).lock do |lock_manager|
-      containers = Docker::Container.all({all: true}, node.docker_connection)
+    puts "sleeping 5 seconds"
+    sleep 5
+    puts "slept"
 
-      attached_slots = []
+    containers = Docker::Container.all({all: true}, node.docker_connection)
 
-      containers.each do |container|
-        lock_manager.keep_locked
+    attached_slots = []
 
-        slot = node.slots.where(container_id: container.id).first
-        if slot
-          attached_slots << slot
-          if container.info["State"] == "exited"
-            if slot.status == "running"
-                # slot.reload
-                # if slot.status == "running" # if slot status STILLS running after locking
-                  slot.releasing!
-                  ReleaseSlotJob.perform_later(slot: MongoidSerializableModel.new(slot))
-                # end
+    containers.each do |container|
+      slot = node.slots.where(container_id: container.id).first
+      if slot
+        attached_slots << slot
+        if container.info["State"] == "exited"
+          if slot.status == "running"
+              # slot.reload
+              # if slot.status == "running" # if slot status STILLS running after locking
+                slot.releasing!
+                ReleaseSlotJob.perform_later(slot: MongoidSerializableModel.new(slot))
               # end
-            end
-          else
-            # UpdateTaskStatusJob.perform_later(Task.find(slot.current_task.id))
+            # end
           end
         else
-          # Here we remove lost containers, like those unknown to container-broker or by some cause
-          # not linked here. But we need to take care to not remove those just created because
-          # there is a sligthly time between their creation and its slot link that cannot be locked
-          # so we remove only containers created at a minimum of 5 minutes
-          if Time.parse(Docker.info(node.docker_connection)["SystemTime"]) - Time.at(container.info["Created"]) > 5.minutes
-            puts "Container #{container.id} not attached with any slot"
-            Rails.logger.info("Container #{container.id} not attached with any slot")
-            # RemoveContainerJob.perform_later(node: node, container_id: container.id)
-          end
+          # UpdateTaskStatusJob.perform_later(Task.find(slot.current_task.id))
+        end
+      else
+        # Here we remove lost containers, like those unknown to container-broker or by some cause
+        # not linked here. But we need to take care to not remove those just created because
+        # there is a sligthly time between their creation and its slot link that cannot be locked
+        # so we remove only containers created at a minimum of 5 minutes
+        if Time.parse(Docker.info(node.docker_connection)["SystemTime"]) - Time.at(container.info["Created"]) > 5.minutes
+          puts "Container #{container.id} not attached with any slot"
+          Rails.logger.info("Container #{container.id} not attached with any slot")
+          # RemoveContainerJob.perform_later(node: node, container_id: container.id)
         end
       end
-
-      zombie_slots = node.slots.where(status: "running") - attached_slots
-      zombie_slots.each do |slot|
-        # ReleaseSlotJob.perform_later(slot: MongoidSerializableModel.new(slot))
-      end
-
-      RunTasksJob.perform_later
     end
+
+    # zombie_slots = node.slots.where(status: "running") - attached_slots
+    # zombie_slots.each do |slot|
+    #   # ReleaseSlotJob.perform_later(slot: MongoidSerializableModel.new(slot))
+    # end
   end
 end
