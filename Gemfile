bbe6711221355742ac06362ede3084fa57964132
Douglas Lise <douglaslise@gmail.com>
2018-05-26 23:43:50 -0300

    feature: Replace meta-http refresh for jquery html replace

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem "jquery-rails"
 
 gem 'docker-api'
 
--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,4 +1,6 @@
 class StatusController < ApplicationController
+  layout proc { false if request.xhr? }
+
   def index
     @nodes = Node.includes(:slots)
     @tasks = Task.includes(slot: :node).to_a
