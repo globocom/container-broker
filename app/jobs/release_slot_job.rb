bf2864c5dcf80d48cdabc0f9128a68150e9478d0
Joao Vieira <joaocv3@gmail.com>
2018-05-28 17:20:04 -0300

    feature: Add feature flag to delete container after run

--- app/jobs/release_slot_job.rb
+++ app/jobs/release_slot_job.rb
@@ -3,7 +3,7 @@ class ReleaseSlotJob < ApplicationJob
 
   def perform(slot:)
     UpdateTaskStatusJob.perform_now(slot.current_task)
-    RemoveContainerJob.perform_later(node: MongoidSerializableModel.new(slot.node), container_id: slot.container_id)
+    RemoveContainerJob.perform_later(node: MongoidSerializableModel.new(slot.node), container_id: slot.container_id) if Settings.delete_container_after_run
     slot.release
     RunTasksJob.perform_later
   end
