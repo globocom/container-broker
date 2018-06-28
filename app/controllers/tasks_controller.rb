4955b101b9d846dc3a98c9af5cfff1b12205398a
Douglas Lise <douglaslise@gmail.com>
2018-06-28 17:57:36 -0300

    feature: Add serializer to Task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,20 +2,17 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :error_log]
 
   def create
-    # todo: implementar mecanismo de seleção de slots
-    slot = Slot.last
-
     @task = Task.new(task_params)
 
     if @task.save
-      render json: @task, except: :_id, status: :created
+      render json: @task
     else
       render json: @task.errors, status: :unprocessable_entity
     end
   end
 
   def show
-    render json: @task, except: :_id
+    render json: @task
   end
 
   def error_log
