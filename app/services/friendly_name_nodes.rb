77ef0f9b27dd4635855425c657a1eb4ccab24b89
Douglas Lise <douglaslise@gmail.com>
2018-07-02 19:21:51 -0300

    chore: Change node naming to use 2 decimal places

--- app/services/friendly_name_nodes.rb
+++ app/services/friendly_name_nodes.rb
@@ -1,9 +1,9 @@
 class FriendlyNameNodes
   def call
     Node.all.to_a.each_with_index do |node, index|
-      node.update(name: "n#{index + 1}")
+      node.update(name: "n#{"%02d" % (index + 1)}")
       node.slots.each_with_index do |slot, index|
-        slot.update(name: "#{node.name}-s#{index + 1}")
+        slot.update(name: "#{node.name}-s#{"%02d" % (index + 1)}")
       end
     end
   end
