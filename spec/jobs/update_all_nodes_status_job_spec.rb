dd4201fc92a04c4ee257ffdce337f43bcebb4c8d
Douglas Lise <douglaslise@gmail.com>
2018-07-16 14:47:39 -0300

    chore: Rename slots in spec

--- spec/jobs/update_all_nodes_status_job_spec.rb
+++ spec/jobs/update_all_nodes_status_job_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe UpdateAllNodesStatusJob, type: :job do
-  let(:node1) { Fabricate(:node, status: "available") }
-  let(:node2) { Fabricate(:node, status: "unstable") }
-  let(:node3) { Fabricate(:node, status: "available") }
-  let(:node4) { Fabricate(:node, status: "unavailable") }
+  let(:node_available_1) { Fabricate(:node, status: "available") }
+  let(:node_unstable) { Fabricate(:node, status: "unstable") }
+  let(:node_available_2) { Fabricate(:node, status: "available") }
+  let(:node_unavailable) { Fabricate(:node, status: "unavailable") }
 
-  let!(:nodes) { [node1, node2, node3] }
+  let!(:nodes) { [node_available_1, node_unstable, node_available_2] }
 
   before { subject.perform }
 
   it "schedule UpdateNodeStatusJob for every available node" do
-    expect(UpdateNodeStatusJob).to have_been_enqueued.with(node: node1)
-    expect(UpdateNodeStatusJob).to have_been_enqueued.with(node: node3)
+    expect(UpdateNodeStatusJob).to have_been_enqueued.with(node: node_available_1)
+    expect(UpdateNodeStatusJob).to have_been_enqueued.with(node: node_available_2)
   end
 
   context "does not schedule UpdateNodeStatusJob" do
     it "for unavailable nodes" do
-      expect(UpdateNodeStatusJob).to_not have_been_enqueued.with(node: node2)
+      expect(UpdateNodeStatusJob).to_not have_been_enqueued.with(node: node_unstable)
     end
 
     it "for unstable nodes" do
-      expect(UpdateNodeStatusJob).to_not have_been_enqueued.with(node: node4)
+      expect(UpdateNodeStatusJob).to_not have_been_enqueued.with(node: node_unavailable)
     end
   end
 end
