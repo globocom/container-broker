ec2ef87d2a0b51eed194c064efc15f4809d51d03
George Secrieru <george.secrieru@gmail.com>
2018-07-30 15:46:18 -0300

    test: Cover log driver options based on Task persist_logs flag

--- spec/jobs/run_task_job_spec.rb
+++ spec/jobs/run_task_job_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe RunTaskJob, type: :job do
 
   let(:node) { Node.create!(hostname: "docker.test") }
-  let(:task) { Task.create!(name: "task-name", image: "#{image}:#{image_tag}", cmd: "-i input.txt -metadata comment='Encoded by Globo.com' output.mp4", storage_mount: "/tmp/workdir") }
+  let(:task_persist_logs) { false }
+  let(:task) { Task.create!(name: "task-name", image: "#{image}:#{image_tag}", cmd: "-i input.txt -metadata comment='Encoded by Globo.com' output.mp4", storage_mount: "/tmp/workdir", persist_logs: task_persist_logs) }
   let(:slot) { Slot.create!(node: node, status: "attaching") }
   let(:image) { "busybox" }
   let(:image_tag) { "3.1" }
@@ -90,18 +91,22 @@ RSpec.describe RunTaskJob, type: :job do
   end
 
   context "when node is available and image exists" do
+    let(:container_log_options) do
+      {
+        "Type" => "fluentd",
+        "Config" => {
+          "tag" => "docker.{{.ID}}",
+          "fluentd-sub-second-precision" => "true"
+        }
+      }
+    end
+
     let(:container_create_options) do
       {
         "Image" => "#{image}:#{image_tag}",
         "HostConfig" => {
           "Binds" => ["/tmp/ef-shared:/tmp/workdir"],
-          "LogConfig" =>  {
-            "Type" => "fluentd",
-            "Config" => {
-              "tag" => "docker.{{.ID}}",
-              "fluentd-sub-second-precision" => "true"
-            }
-          },
+          "LogConfig" =>  container_log_options,
           "NetworkMode"=> ""
         },
         "Entrypoint" => [],
@@ -189,6 +194,16 @@ RSpec.describe RunTaskJob, type: :job do
     it "calls node update_usage" do
       expect(node).to receive(:update_usage).at_least(1).times
       perform
+    end
+
+    context "and task is configured to persist logs" do
+      let(:task_persist_logs) { true }
+      let(:container_log_options) { { "Type" => "json-file" } }
+
+      it "creates container with json-file log options" do
+        expect(Docker::Container).to receive(:create).with(container_create_options, a_kind_of(Docker::Connection))
+        perform
+      end
     end
   end
 end
