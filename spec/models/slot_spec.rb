358b7e8832fbd3753a6f4b101ab348c22e13c4ef
Luis Choma <luis.choma@corp.globo.com>
2019-07-25 14:32:12 -0300

    feature: Add validation to slot execution types names on node creation

--- spec/models/slot_spec.rb
+++ spec/models/slot_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Slot, type: :model do
+  context "when creating slots" do
+    let (:execution_type) { "defined-in-each-context" }
+    subject { Fabricate(:slot, execution_type: execution_type) }
+
+    context "with a valid execution type" do
+      let(:execution_type) { "io" }
+      it "is valid" do
+        expect(subject).to be_valid
+      end
+    end
+
+    context "with an invalid execution type" do
+      let(:execution_type) { "invalid_symbol" }
+
+      it "raises validation error" do
+        expect{subject}.to raise_error(Mongoid::Errors::Validations)
+      end
+    end
+  end
 end
