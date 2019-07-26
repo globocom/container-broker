bbe9a15e67a6787a137759709b9456703d1f1590
Joao Vieira <joaocv3@gmail.com>
2019-07-26 15:52:00 -0300

    feature: Show execution type usage in dashboard

--- app/serializers/status_panel_node_serializer.rb
+++ app/serializers/status_panel_node_serializer.rb
@@ -1,4 +1,4 @@
 class StatusPanelNodeSerializer < ActiveModel::Serializer
-  attributes :uuid, :name, :hostname, :cores, :memory, :status, :last_error, :last_success_at
+  attributes :uuid, :name, :hostname, :status, :last_error, :last_success_at, :usage_per_execution_type
   has_many :slots, serializer: StatusPanelSlotSerializer
 end
