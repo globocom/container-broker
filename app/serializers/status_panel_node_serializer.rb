fbfb7b64b6bfd14ec19516865c3b5db957153262
Douglas Lise <douglaslise@gmail.com>
2019-07-29 17:24:41 -0300

    feature: Add slot execution types in dashboard

--- app/serializers/status_panel_node_serializer.rb
+++ app/serializers/status_panel_node_serializer.rb
@@ -1,5 +1,6 @@
 class StatusPanelNodeSerializer < ActiveModel::Serializer
-  attributes :uuid, :name, :hostname, :status, :last_error, :last_success_at, :usage_per_execution_type, :accept_new_tasks
+  attributes :uuid, :name, :hostname, :status, :last_error, :last_success_at,
+    :usage_per_execution_type, :slots_execution_types, :accept_new_tasks
 
   has_many :slots, serializer: StatusPanelSlotSerializer
 end
