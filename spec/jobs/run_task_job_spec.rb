bbc3a58861e8e4a2121a74aeb48167ded535383e
Luis Choma <luis.choma@corp.globo.com>
2019-07-29 18:07:58 -0300

    test: Fix broken tests due to validation on slot model

--- spec/jobs/run_task_job_spec.rb
+++ spec/jobs/run_task_job_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe RunTaskJob, type: :job do
 
   let(:node) { Node.create!(hostname: "docker.test") }
   let(:task) { Task.create!(name: "task-name", image: "#{image}:#{image_tag}", cmd: "-i input.txt -metadata comment='Encoded by Globo.com' output.mp4", storage_mount: "/tmp/workdir") }
-  let(:slot) { Slot.create!(node: node, status: "attaching") }
+  let(:slot) { Slot.create!(node: node, status: "attaching", execution_type: "test") }
   let(:image) { "busybox" }
   let(:image_tag) { "3.1" }
 
