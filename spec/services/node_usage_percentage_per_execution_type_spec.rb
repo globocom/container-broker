d0b13af28ebbd222fff2facd3fc66f23bab2bd34
Joao Vieira <joaocv3@gmail.com>
2019-07-29 10:51:01 -0300

    refactor: Create slots inside the before block

--- spec/services/node_usage_percentage_per_execution_type_spec.rb
+++ spec/services/node_usage_percentage_per_execution_type_spec.rb
@@ -3,15 +3,17 @@ require 'rails_helper'
 RSpec.describe NodeUsagePercentagePerExecutionType, type: :service do
   let(:node1) { Fabricate(:node, name: "node1") }
 
-  let!(:slot1_1) { Fabricate(:slot, node: node1, name: "slot1_1", execution_type: "io") }
-  let!(:slot1_2) { Fabricate(:slot, node: node1, name: "slot1_3", execution_type: "io") }
-  let!(:slot1_3) { Fabricate(:slot, node: node1, name: "slot1_4", execution_type: "network", status: "running") }
-  let!(:slot1_4) { Fabricate(:slot, node: node1, name: "slot1_5", execution_type: "network") }
-  let!(:slot1_5) { Fabricate(:slot, node: node1, name: "slot1_5", execution_type: "cpu", status: "running") }
-  let!(:slot1_6) { Fabricate(:slot, node: node1, name: "slot1_6", execution_type: "cpu") }
-  let!(:slot1_7) { Fabricate(:slot, node: node1, name: "slot1_7", execution_type: "cpu") }
-  let!(:slot1_8) { Fabricate(:slot, node: node1, name: "slot1_8", execution_type: "cpu") }
-  let!(:slot1_9) { Fabricate(:slot, node: node1, name: "slot1_9", execution_type: "cpu") }
+  before do
+    Fabricate(:slot, node: node1, name: "slot1_1", execution_type: "io")
+    Fabricate(:slot, node: node1, name: "slot1_3", execution_type: "io")
+    Fabricate(:slot, node: node1, name: "slot1_4", execution_type: "network", status: "running")
+    Fabricate(:slot, node: node1, name: "slot1_5", execution_type: "network")
+    Fabricate(:slot, node: node1, name: "slot1_5", execution_type: "cpu", status: "running")
+    Fabricate(:slot, node: node1, name: "slot1_6", execution_type: "cpu")
+    Fabricate(:slot, node: node1, name: "slot1_7", execution_type: "cpu")
+    Fabricate(:slot, node: node1, name: "slot1_8", execution_type: "cpu")
+    Fabricate(:slot, node: node1, name: "slot1_9", execution_type: "cpu")
+  end
 
   context "with a given node" do
     it "returns the percentage for the slots of the node" do
