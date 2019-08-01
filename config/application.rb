3c5c85c51b442432cd62af1d2bd7c082edae3923
Joao Vieira <joaocv3@gmail.com>
2019-08-01 17:37:02 -0300

    chore: Add cors

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,11 @@ module ContainerBroker
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # enable cors
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => 'GET, PATCH, PUT, POST, OPTIONS, DELETE',
+    }
   end
 end
