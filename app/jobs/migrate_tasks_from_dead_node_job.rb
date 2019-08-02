7b5430d612ffee4cf4f56bf468bdeab8a0ecfe08
Douglas Lise <douglaslise@gmail.com>
2019-08-02 14:54:54 -0300

    feature: Add lock in MigrateTasksFromDeadNodeJob

--- app/jobs/migrate_tasks_from_dead_node_job.rb
+++ app/jobs/migrate_tasks_from_dead_node_job.rb
@@ -2,20 +2,22 @@ class MigrateTasksFromDeadNodeJob < ApplicationJob
   queue_as :default
 
   def perform(node:)
-    Rails.logger.debug("Migrating tasks from #{node}")
-    node.slots.reject(&:idle?).each do |slot|
-      Rails.logger.debug("Migrating task for #{slot}")
-      current_task = slot.current_task
-      if current_task
-        Rails.logger.debug("Retrying slot current task #{current_task}")
-        current_task.retry if current_task.starting? || current_task.started? || current_task.running?
-      else
-        Rails.logger.debug("Slot does not have current task")
-      end
+    LockManager.new(type: self.class.to_s, id: node.id, wait: false, expire: 1.minute).lock do
+      Rails.logger.debug("Migrating tasks from #{node}")
+      node.slots.reject(&:idle?).each do |slot|
+        Rails.logger.debug("Migrating task for #{slot}")
+        current_task = slot.current_task
+        if current_task
+          Rails.logger.debug("Retrying slot current task #{current_task}")
+          current_task.retry if current_task.starting? || current_task.started? || current_task.running?
+        else
+          Rails.logger.debug("Slot does not have current task")
+        end
 
-      Rails.logger.debug("Releasing #{slot}")
-      slot.release
-      Rails.logger.debug("#{slot} released")
+        Rails.logger.debug("Releasing #{slot}")
+        slot.release
+        Rails.logger.debug("#{slot} released")
+      end
     end
   end
 end
