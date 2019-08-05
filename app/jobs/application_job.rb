8cc5bf9b6c939251dbd1b9294cc56ce9fdcbc36e
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2019-08-05 17:50:23 -0300

    feature: Add basic jobs metrics

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,3 +1,15 @@
 class ApplicationJob < ActiveJob::Base
+  JOB_METRIC = "jobs"
 
+  around_perform do |job, block|
+    Metrics.new(JOB_METRIC).duration do |metric|
+      metric[:job_id] = job.job_id
+      metric[:job_class] = job.class.to_s
+      metric[:executions] = job.executions
+      metric[:queue_name] = job.queue_name
+      metric[:hostname] = `hostname`.strip
+
+      block.call
+    end
+  end
 end
