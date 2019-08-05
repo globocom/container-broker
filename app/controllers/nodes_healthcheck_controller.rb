4e8b87e7f63c86887eb1d4efeb10428bf6245e87
Luis Choma <luis.choma@corp.globo.com>
2019-08-05 18:07:47 -0300

    refactor: Change 'invalid' to 'failed'

--- app/controllers/nodes_healthcheck_controller.rb
+++ app/controllers/nodes_healthcheck_controller.rb
@@ -2,22 +2,22 @@ class NodesHealthcheckController < ApplicationController
   def index
     render json: {
       status: status,
-      failed_nodes: invalid_nodes.map{ |nodes| NodeHealthcheckSerializer.new(nodes) }
+      failed_nodes: failed_nodes.map{ |node| NodeHealthcheckSerializer.new(node) }
     }
   end
 
   private
 
-  def invalid_nodes
-    @invalid_nodes ||= Node.not.available
+  def failed_nodes
+    @failed_nodes ||= Node.not.available
   end
 
-  def nodes_invalid?
-    invalid_nodes.to_a.any?
+  def nodes_failed?
+    failed_nodes.to_a.any?
   end
 
   def status
-    if nodes_invalid?
+    if nodes_failed?
       "FAILING"
     else
       "WORKING"
--- app/controllers/tasks_healthcheck_controller.rb
+++ app/controllers/tasks_healthcheck_controller.rb
@@ -2,20 +2,22 @@ class TasksHealthcheckController < ApplicationController
   def index
     render json: {
       status: status,
-      failed_tasks: invalid_tasks.map{ |task| TaskHealthcheckSerializer.new(task) },
+      failed_tasks: failed_tasks.map{ |task| TaskHealthcheckSerializer.new(task) },
     }
   end
 
-  def invalid_tasks
-    @invalid_tasks ||= Task.error
+  private
+
+  def failed_tasks
+    @failed_tasks ||= Task.error
   end
 
-  def tasks_invalid?
-    invalid_tasks.to_a.any?
+  def tasks_failed?
+    failed_tasks.to_a.any?
   end
 
   def status
-    if tasks_invalid?
+    if tasks_failed?
       "FAILING"
     else
       "WORKING"
