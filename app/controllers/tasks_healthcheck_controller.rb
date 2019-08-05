4f3781f27f72641157da6a7dcbba1ad235a15254
Luis Choma <luis.choma@corp.globo.com>
2019-08-05 16:55:45 -0300

    chore: Remove unused line

--- app/controllers/tasks_healthcheck_controller.rb
+++ app/controllers/tasks_healthcheck_controller.rb
@@ -2,8 +2,6 @@ class TasksHealthcheckController < ApplicationController
   before_action only: %i[index]
 
   def index
-    failed_tasks = Task.where(:status => "error")
-
     render json: {
       status: status,
       failed_tasks: invalid_tasks.map{ |task| TaskHealthcheckSerializer.new(task) },
