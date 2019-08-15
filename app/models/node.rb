9eb3fabfb661843b5f108d7500c247b24e8e8f35
Douglas Lise <douglaslise@gmail.com>
2019-08-15 15:53:57 -0300

    refactor: Move Docker logger config to an initializer

--- app/models/node.rb
+++ app/models/node.rb
@@ -42,7 +42,6 @@ class Node
   end
 
   def docker_connection
-    Docker.logger = Logger.new(STDOUT)
     Docker::Connection.new(hostname, {connect_timeout: 10, read_timeout: 10, write_timeout: 10})
   end
 
---
+++ config/initializers/docker_config.rb
@@ -0,0 +1,1 @@
+Docker.logger = Logger.new(STDOUT)
