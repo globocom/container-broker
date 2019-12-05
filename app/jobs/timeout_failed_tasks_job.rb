648aed285351659d2d2228f3632b647ec63d83f0
Luis Choma <luis.choma@corp.globo.com>
2019-12-05 15:43:49 -0300

    feature: Add timeout jobs for failed tasks

---
+++ app/jobs/timeout_failed_tasks_job.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class TimeoutFailedTasksJob < ApplicationJob
+  queue_as :default
+
+  def perform(*_args)
+    timeout_failed_tasks
+  end
+
+  def timeout_failed_tasks
+    tasks = failed_tasks
+
+    tasks.map do |task|
+      if task_live_time(task.finished_at) >= Settings.timeout_tasks_after_hours
+        task.error!
+      end
+    end
+  end
+
+  def failed_tasks
+    Task.where(status: "failed").to_a
+  end
+
+  def task_live_time(finished_at)
+    (Time.parse(Time.zone.now.to_datetime.to_s) - Time.parse(finished_at.to_s))/3_600
+  end
+end
---
+++ spec/jobs/timeout_failed_tasks_job_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe TimeoutFailedTasksJob, type: :job do
+  let(:task) { Fabricate(:task, status: "failed", finished_at: finished_at) }
+  let(:finished_at) { "Defined in each context" }
+  let(:now_time) { "Thu, 05 Dec 2019 23:59:00 +0000 "}
+  let(:perform) { subject.perform }
+
+  before do
+    allow(Task).to receive(:where).and_return(task)
+  end
+
+  context "when task failed less than 20 hours ago" do
+    let(:finished_at) { "Thu, 05 Dec 2019 23:50:00 +0000" }
+
+    it "keeps task as failed" do
+      perform
+      expect(task.status).to be("failed")
+    end
+  end
+
+  context "when task failed more than 20 hours ago" do
+    let(:finished_at) { "Thu, 03 Dec 2019 23:50:00 +0000" }
+
+    it "marks the task as error" do
+      perform
+      expect(task.status).to be("error")
+    end
+  end
+end
