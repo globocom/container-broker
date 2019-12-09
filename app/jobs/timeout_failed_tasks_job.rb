dedc0a4805c5712e71c4cd432244c0f16f6711fc
Luis Choma <luis.choma@corp.globo.com>
2019-12-09 16:02:18 -0300

    refactor: Change timeout job class methods

--- app/jobs/timeout_failed_tasks_job.rb
+++ app/jobs/timeout_failed_tasks_job.rb
@@ -8,22 +8,14 @@ class TimeoutFailedTasksJob < ApplicationJob
   end
 
   def timeout_failed_tasks
-    tasks = failed_tasks
-
-    tasks.map do |task|
-      next unless task_live_time(task.finished_at) >= Settings.timeout_tasks_after_hours
-
+    failed_tasks_to_timeout.map do |task|
       Rails.logger.debug("Marking task as error due to timeout: #{task.uuid}")
 
       task.error!
     end
   end
 
-  def failed_tasks
-    Task.where(status: "failed").to_a
-  end
-
-  def task_live_time(finished_at)
-    (Time.parse(Time.zone.now.to_datetime.to_s) - Time.parse(finished_at.to_s)) / 3_600
+  def failed_tasks_to_timeout
+    Task.failed.where(:finished_at.lt => Time.current - Settings.timeout_tasks_after_hours)
   end
 end
