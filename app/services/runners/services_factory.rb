db3898f6089592580a25ebd38d5467d8a77fdf73
Mateus Nava <nava.mateus@gmail.com>
2020-01-17 17:42:50 -0300

    refactor: Create fabricator for remove container

--- app/services/runners/services_factory.rb
+++ app/services/runners/services_factory.rb
@@ -10,14 +10,16 @@ module Runners
         monitor_unresponsive_node: Runners::Kubernetes::MonitorUnresponsiveNode,
         run_task: Runners::Kubernetes::RunTask,
         kill_slot_container: Runners::Kubernetes::KillSlotContainer,
-        fetch_task_container: Runners::Kubernetes::FetchTaskContainer
+        fetch_task_container: Runners::Kubernetes::FetchTaskContainer,
+        remove_container: Runners::Kubernetes::RemoveContainer
       },
       docker: {
         update_node_status: Runners::Docker::UpdateNodeStatus,
         monitor_unresponsive_node: Runners::Docker::MonitorUnresponsiveNode,
         run_task: Runners::Docker::RunTask,
         kill_slot_container: Runners::Docker::KillSlotContainer,
-        fetch_task_container: Runners::Docker::FetchTaskContainer
+        fetch_task_container: Runners::Docker::FetchTaskContainer,
+        remove_container: Runners::Docker::RemoveContainer
       }
     }.freeze
 
