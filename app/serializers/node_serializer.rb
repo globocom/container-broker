6c002f2b159082a44373df641e870fe89e827180
Mateus Nava <nava.mateus@gmail.com>
2020-01-23 17:02:20 -0300

    feature: Add runner field in the serializer

--- app/serializers/node_serializer.rb
+++ app/serializers/node_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class NodeSerializer < ActiveModel::Serializer
-  attributes :uuid, :hostname, :status, :accept_new_tasks, :slots_execution_types
+  attributes :uuid, :hostname, :status, :accept_new_tasks, :slots_execution_types, :runner
 end
