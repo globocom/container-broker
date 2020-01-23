0a8e837b0e2503732c21f735d43c7daa5c13fa4f
Mateus Nava <nava.mateus@gmail.com>
2020-01-23 15:09:08 -0300

    refactor: Uses create execution info

--- app/services/runners/docker/fetch_execution_info.rb
+++ app/services/runners/docker/fetch_execution_info.rb
@@ -4,26 +4,11 @@ module Runners
   module Docker
     class FetchExecutionInfo
       def perform(task:)
-        info = ServicesFactory
-               .fabricate(runner: :docker, service: :fetch_task_container)
-               .perform(task: task)
-               .info
+        container = ServicesFactory
+                    .fabricate(runner: :docker, service: :fetch_task_container)
+                    .perform(task: task)
 
-        create_execution_info(info)
-      end
-
-      private
-
-      def create_execution_info(info)
-        state = info["State"]
-        Runners::ExecutionInfo.new(
-          id: info["id"],
-          status: state["Status"],
-          exit_code: state["ExitCode"],
-          started_at: state["StartedAt"],
-          finished_at: state["FinishedAt"],
-          error: state["Error"]
-        )
+        CreateExecutionInfo.new.perform(container: container)
       end
     end
   end
--- spec/services/runners/docker/fetch_execution_info_spec.rb
+++ spec/services/runners/docker/fetch_execution_info_spec.rb
@@ -3,56 +3,22 @@
 require "rails_helper"
 
 RSpec.describe Runners::Docker::FetchExecutionInfo, type: :service do
-  let(:fetch_task_container_instance) { double(Runners::Docker::FetchTaskContainer) }
-  let(:docker_container_instance) { double(::Docker::Container, info: info) }
-  let(:info) do
-    {
-      "id" => "id",
-      "State" => {
-        "Status" => "Exited",
-        "ExitCode" => 0,
-        "StartedAt" => Time.current.to_s,
-        "FinishedAt" => (Time.current + 1.minute).to_s,
-        "Error" => "Error"
-      }
-    }
-  end
-  let(:task) { Fabricate(:task) }
+  let(:container_id) { "df782008cf45" }
+  let(:node) { Fabricate(:node) }
+  let(:slot) { Fabricate(:slot_running, node: node) }
+  let(:task) { Fabricate(:task, container_id: container_id, slot: slot) }
+  let(:container) { double(Docker::Container) }
 
   before do
-    allow(Runners::ServicesFactory).to receive(:fabricate)
-      .with(runner: :docker, service: :fetch_task_container)
-      .and_return(fetch_task_container_instance)
-    allow(fetch_task_container_instance).to receive(:perform)
-      .with(task: task)
-      .and_return(docker_container_instance)
+    allow(::Docker::Container).to receive(:get)
+      .with(container_id, anything, anything)
+      .and_return(container)
   end
 
-  subject { described_class.new.perform(task: task) }
+  it "creates an execution info" do
+    expect_any_instance_of(Runners::Docker::CreateExecutionInfo).to receive(:perform)
+      .with(container: container)
 
-  context "creates an execution info" do
-    it "with id" do
-      expect(subject.id).to eq(info["id"])
-    end
-
-    it "with status" do
-      expect(subject.status).to eq(info["State"]["Status"])
-    end
-
-    it "with exit code" do
-      expect(subject.exit_code).to eq(info["State"]["ExitCode"])
-    end
-
-    it "with started at" do
-      expect(subject.started_at).to eq(info["State"]["StartedAt"])
-    end
-
-    it "with finished at" do
-      expect(subject.finished_at).to eq(info["State"]["FinishedAt"])
-    end
-
-    it "with error" do
-      expect(subject.error).to eq(info["State"]["Error"])
-    end
+    subject.perform(task: task)
   end
 end
