c541f16d180e6537b51cd37d70e1ef64f275c307
Mateus Nava <nava.mateus@gmail.com>
2020-01-23 10:28:47 -0300

    refactor: Use struct instead meta programming

--- app/services/runners/execution_info.rb
+++ app/services/runners/execution_info.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 module Runners
-  class ExecutionInfo
-    attr_accessor :id, :status, :exit_code, :started_at, :finished_at, :error
-
-    def initialize(values = {})
-      values.each { |key, value| send("#{key}=", value) }
-    end
-  end
+  ExecutionInfo = Struct.new(:id, :status, :exit_code, :started_at, :finished_at, :error, keyword_init: true)
 end
