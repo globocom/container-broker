95d0a2e4562dae911b0deb6d297728e334088106
Mateus Nava <nava.mateus@gmail.com>
2020-01-23 15:23:54 -0300

    test: Improve coverage of spec

--- spec/services/runners/docker/fetch_execution_info_spec.rb
+++ spec/services/runners/docker/fetch_execution_info_spec.rb
@@ -3,22 +3,40 @@
 require "rails_helper"
 
 RSpec.describe Runners::Docker::FetchExecutionInfo, type: :service do
-  let(:container_id) { "df782008cf45" }
   let(:node) { Fabricate(:node) }
   let(:slot) { Fabricate(:slot_running, node: node) }
-  let(:task) { Fabricate(:task, container_id: container_id, slot: slot) }
+  let(:task) { Fabricate(:task, slot: slot) }
   let(:container) { double(Docker::Container) }
+  let(:fetch_task_container_instance) { double(Runners::Docker::FetchTaskContainer) }
+  let(:execution_info) { double(Runners::ExecutionInfo) }
 
   before do
-    allow(::Docker::Container).to receive(:get)
-      .with(container_id, anything, anything)
+    allow(Runners::ServicesFactory).to receive(:fabricate)
+      .with(runner: :docker, service: :fetch_task_container)
+      .and_return(fetch_task_container_instance)
+
+    allow(fetch_task_container_instance).to receive(:perform)
+      .with(task: task)
       .and_return(container)
   end
 
-  it "creates an execution info" do
+  it "uses CreateExecutionInfo service" do
     expect_any_instance_of(Runners::Docker::CreateExecutionInfo).to receive(:perform)
       .with(container: container)
+      .and_return(execution_info)
 
     subject.perform(task: task)
+  end
+
+  context "creates an execution info" do
+    before do
+      allow_any_instance_of(Runners::Docker::CreateExecutionInfo).to receive(:perform)
+        .with(container: container)
+        .and_return(execution_info)
+    end
+
+    it "creates an execution info" do
+      expect(subject.perform(task: task)).to eq(execution_info)
+    end
   end
 end
