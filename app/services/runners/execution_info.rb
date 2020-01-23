1f732aeac45585d10787a32baf2ee1f7b089fc21
Douglas Lise <douglaslise@gmail.com>
2020-01-23 15:31:05 -0300

    feature: Change ExecutionInfo to have attributes

--- app/services/runners/execution_info.rb
+++ app/services/runners/execution_info.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
 module Runners
-  ExecutionInfo = Struct.new(:id, :status, :exit_code, :started_at, :finished_at, :error, keyword_init: true)
+  class ExecutionInfo < Struct.new(:id, :status, :exit_code, :started_at, :finished_at, :error, keyword_init: true)
+    def success?
+      status == "success"
+    end
+
+    def error?
+      status == "error"
+    end
+
+    def running?
+      status == "running"
+    end
+
+    def pending?
+      status == "pending"
+    end
+
+    def terminated?
+      success? || error?
+    end
+  end
 end
