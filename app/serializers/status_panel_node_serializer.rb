a37cc08f69a4e1c440948dd51188c748e8282998
Douglas Lise <douglaslise@gmail.com>
2020-01-28 16:02:20 -0300

    chore: Add new fields to node serializer

--- app/serializers/status_panel_node_serializer.rb
+++ app/serializers/status_panel_node_serializer.rb
@@ -2,7 +2,8 @@
 
 class StatusPanelNodeSerializer < ActiveModel::Serializer
   attributes :uuid, :name, :hostname, :status, :last_error, :last_success_at,
-             :usage_per_execution_type, :slots_execution_types, :accept_new_tasks
+             :usage_per_execution_type, :slots_execution_types, :accept_new_tasks,
+             :runner, :runner_capacity_reached
 
   has_many :slots, serializer: StatusPanelSlotSerializer
 end
