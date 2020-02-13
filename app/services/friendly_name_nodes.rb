92e6748efb77924aadf95bbc6043a73b4148d5e7
Mateus Nava <nava.mateus@gmail.com>
2020-02-13 10:12:58 -0300

    fix: Change query to use runner provider

--- app/services/friendly_name_nodes.rb
+++ app/services/friendly_name_nodes.rb
@@ -2,7 +2,7 @@
 
 class FriendlyNameNodes
   def perform
-    Node.order(runner: :desc, hostname: :asc, id: :asc).each_with_index do |node, index|
+    Node.order(runner_provider: :desc, hostname: :asc, id: :asc).each_with_index do |node, index|
       node.update(name: "n#{format("%02d%s", (index + 1), node.runner_provider.first)}")
       FriendlyNameSlots.new(node: node).perform
     end
