5127885976dac408ad814b952dc7d47eaa963bf3
Douglas Lise <douglaslise@gmail.com>
2020-04-06 15:12:49 -0300

    feature: Change user used in containers to be the same that is used in EF

--- app/services/kubernetes_client.rb
+++ app/services/kubernetes_client.rb
@@ -37,7 +37,11 @@ class KubernetesClient
                 requests: { cpu: Settings.kubernetes.requests.cpu, memory: Settings.kubernetes.requests.memory },
                 limits: { cpu: Settings.kubernetes.limits.cpu, memory: Settings.kubernetes.limits.memory }
               },
-              volumeMounts: internal_mounts
+              volumeMounts: internal_mounts,
+              securityContext: {
+                runAsUser: Settings.run_container_as.user_id,
+                runAsGroup: Settings.run_container_as.group_id
+              }
             }
           ],
           restartPolicy: "Never",
