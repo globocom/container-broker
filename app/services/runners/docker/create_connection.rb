528dadca90b8dd1cce122d0679c1be391a30dffc
Douglas Lise <douglaslise@gmail.com>
2020-07-27 15:05:33 -0300

    feature: Reduce docker timeout from 10 to 5 seconds

--- app/services/runners/docker/create_connection.rb
+++ app/services/runners/docker/create_connection.rb
@@ -6,7 +6,7 @@ module Runners
       def perform(node:)
         raise(Runners::InvalidRunner, "Node must be a docker runner") unless node.docker?
 
-        ::Docker::Connection.new(node.hostname, connect_timeout: 10, read_timeout: 10, write_timeout: 10)
+        ::Docker::Connection.new(node.hostname, connect_timeout: 5, read_timeout: 5, write_timeout: 5)
       end
     end
   end
