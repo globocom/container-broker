21e66d80f67f446c355707d065bb6f8365448946
Douglas Lise <douglaslise@gmail.com>
2020-08-11 14:57:00 -0300

    chore: Change docker connection syntax

--- app/services/runners/docker/create_connection.rb
+++ app/services/runners/docker/create_connection.rb
@@ -6,7 +6,12 @@ module Runners
       def perform(node:)
         raise(Runners::InvalidRunner, "Node must be a docker runner") unless node.docker?
 
-        ::Docker::Connection.new(node.hostname, connect_timeout: 5, read_timeout: 5, write_timeout: 5)
+        ::Docker::Connection.new(
+          node.hostname,
+          connect_timeout: 5,
+          read_timeout: 5,
+          write_timeout: 5
+        )
       end
     end
   end
