92a4aa194307991985b574c297e5d75ec3759da7
Douglas Lise <douglaslise@gmail.com>
2020-08-13 17:16:25 -0300

    refactor: Change sidekiq scheduler to load explicitly

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,33 +4,18 @@ Rails.application.config.active_job.queue_adapter = Rails.env.test? ? :test : :s
 
 Sidekiq.default_worker_options = { backtrace: true }
 
-def redis_from_url(uri)
-  if uri.start_with?("sentinel")
-    m = uri.match("sentinel://:([^@]*)@([^/]*)/service_name:(.*)")
-    password = m[1]
-    sentinel_uris = m[2]
-    name = m[3]
-    url = "redis://:#{password}@#{name}"
-    sentinels = sentinel_uris.split(",").map do |sentinel_uri|
-      host, port = sentinel_uri.split(":")
-      {
-        host: host,
-        port: port
-      }
-    end
-    Redis.new(url: url, sentinels: sentinels)
-  else
-    Redis.new(url: uri)
-  end
-end
-
 connection = proc {
-  redis_from_url(Settings.redis_url)
+  Redis.new(RedisUrlParser.call(Settings.redis_url))
 }
 
 Sidekiq.configure_server do |config|
   config.redis = ConnectionPool.new(size: 50, &connection)
   config.logger.level = Logger::DEBUG
+
+  config.on(:startup) do
+    Sidekiq.schedule = YAML.load_file(File.expand_path("../sidekiq_scheduler.yml", __dir__))
+    SidekiqScheduler::Scheduler.instance.reload_schedule!
+  end
 end
 
 Sidekiq.configure_client do |config|
