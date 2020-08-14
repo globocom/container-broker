6a58e8b786df9f8b197f85c978e194bb70099d3c
Douglas Lise <douglaslise@gmail.com>
2020-08-14 11:49:32 -0300

    test: Add tests for observable

---
+++ spec/observers/observable_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Observable do
+  subject { observable_class.new }
+  let(:observable_class) do
+    Class.new do
+      extend Observable
+    end
+  end
+  let(:observer_class) { double }
+  let(:observer) { double }
+
+  before do
+    allow(observer_class).to receive(:new).with(subject).and_return(observer)
+  end
+
+  it "accepts observers" do
+    observable_class.add_observer(observer_class)
+
+    expect(observable_class.observers).to include(observer_class)
+  end
+
+  it "creates observer instances" do
+    observable_class.add_observer(observer_class)
+
+    expect(observable_class.observer_instances_for(subject)).to include(observer)
+  end
+end
