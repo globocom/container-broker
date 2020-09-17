137ab116e63e22b8b22e980acb299ad8382f54fa
Mateus Nava <nava.mateus@gmail.com>
2020-09-17 10:49:03 -0300

    fix: Adjust attribute on serializer

--- app/serializers/status_panel_task_serializer.rb
+++ app/serializers/status_panel_task_serializer.rb
@@ -3,7 +3,7 @@
 class StatusPanelTaskSerializer < ActiveModel::Serializer
   attributes :uuid, :name, :image, :cmd, :status, :exit_code, :error, :try_count, :created_at,
              :started_at, :finished_at, :progress, :seconds_running, :tags, :runner_id,
-             :storage_mount, :ingest_storage_mount, :slot, :execution_type
+             :storage_mounts, :slot, :execution_type
 
   def slot
     if object.slot
